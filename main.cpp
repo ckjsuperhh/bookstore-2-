9073a0a9533a9a8254d5c773342c0c8be9de11d9
ckjsuperhh <846230484@qq.com>
2024-12-31 23:45:21 +0800

    revise(1)
    in case of tle,we will return 0 if it read eof

--- main.cpp
+++ main.cpp
@@ -7,8 +7,13 @@ using namespace std;
    cmd::initialise();
    while (true) {
       try {
+         bool ok=true;
          while (getline(cin,str)) {
+            ok=false;
             cmd::analyse(str);
+         }
+         if (ok) {
+            return 0;
          }
       }catch (...) {
          cout<<"invalid\n";
